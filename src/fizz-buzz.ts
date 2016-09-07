7ac9094995c791531d3076504c160ef85df291d9
Sebastián Estrella <sebas007estrella@gmail.com>
2016-09-06 21:52:10 -0500

    Third case when number is divisible by 3 and 5

--- src/fizz-buzz.ts
+++ src/fizz-buzz.ts
@@ -1,6 +1,9 @@
 "use strict";
 
 export default function(num: number): string {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "fizz buzz";
+  }
   if (num % 3 === 0) {
     return "fizz";
   }
--- test/fizz-buzz.test.ts
+++ test/fizz-buzz.test.ts
@@ -16,4 +16,10 @@ describe("fizzBuzz", function() {
       expect(fizzBuzz(5)).to.be.equal("buzz");
     });
   });
+
+  context("when the number is divisible by 3 and 5", function() {
+    it("returns fizz buzz", function() {
+      expect(fizzBuzz(15)).to.be.equal("fizz buzz");
+    });
+  });
 });
